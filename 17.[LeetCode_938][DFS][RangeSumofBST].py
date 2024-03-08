3a2e8f293b38fb0db23f3b76ab6381485a3204c1
JaeWan Park <jeantirole01@gmail.com>
2024-03-08 11:33:37 +0900

    Update 17.[LeetCode_938][DFS][RangeSumofBST].py

--- 17.[LeetCode_938][DFS][RangeSumofBST].py
+++ 17.[LeetCode_938][DFS][RangeSumofBST].py
@@ -53,4 +53,30 @@ def dfs(tree_node):
 dfs(tree)
 
 print(sum)
-  
+
+
+#---- for LeetCode platform : should use class variable self.var
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    sum = 0
+
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if root is None:
+            return 0
+        
+        if low <= root.val <= high:
+            self.sum += root.val
+
+        if root.left:
+            self.rangeSumBST(root.left, low, high)
+
+        if root.right:
+            self.rangeSumBST(root.right, low, high)
+
+        return self.sum
