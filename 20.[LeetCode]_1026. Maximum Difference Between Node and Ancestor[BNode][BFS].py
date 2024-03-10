25b0b80ad934a8815f2a2dc2c396373213cfd2b3
JaeWan Park <jeantirole01@gmail.com>
2024-03-10 23:15:00 +0900

    Update 20.[LeetCode]_1026. Maximum Difference Between Node and Ancestor[BNode][BFS].py

--- 20.[LeetCode]_1026. Maximum Difference Between Node and Ancestor[BNode][BFS].py
+++ 20.[LeetCode]_1026. Maximum Difference Between Node and Ancestor[BNode][BFS].py
@@ -1,5 +1,7 @@
 #-------------------------------------------------- Problem
 1026. Maximum Difference Between Node and Ancestor
+Link : https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+
 
 Given the root of a binary tree, find the maximum value v for which there exist different nodes a and b where v = |a.val - b.val| and a is an ancestor of b.
 
