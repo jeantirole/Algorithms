9fdd63ed4a8cd550af8f9d2c4b90e4546587b392
JaeWan Park <jeantirole01@gmail.com>
2024-03-29 23:52:45 +0900

    Create [LeetCode][Graph]547. Number of Provinces.py

---
+++ [LeetCode][Graph]547. Number of Provinces.py
@@ -0,0 +1,37 @@
+# Pb --------------------
+https://leetcode.com/problems/number-of-provinces/description/
+
+547. Number of Provinces
+
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+
+ 
+
+Example 1:
+
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2
+
+
+# Sol --------------------
+def findCircleNum(self, A):
+    N = len(A)
+    seen = set()
+    def dfs(node):
+        for nei, adj in enumerate(A[node]):
+            if adj and nei not in seen:
+                seen.add(nei)
+                dfs(nei)
+    
+    ans = 0
+    for i in xrange(N):
+        if i not in seen:
+            dfs(i)
+            ans += 1
+    return ans
