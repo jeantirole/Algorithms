6c98ef8efd463a398d1e29e93c867a7608f516f9
JaeWan Park <jeantirole01@gmail.com>
2024-07-07 12:34:30 +0900

    Update 14888. 연산자 끼워넣기 (백준, 파이썬).py

--- 14888. 연산자 끼워넣기 (백준, 파이썬).py
+++ 14888. 연산자 끼워넣기 (백준, 파이썬).py
@@ -27,3 +27,8 @@ add, sub, mul, div = map(int, input().split())
 mn, mx = int(1e9), int(-1e9)
 dfs(1, lst[0], add, sub, mul, div)
 print(mx, mn, sep='\n')
+
+# 시간복잡도
+최악의 경우, 모든 연산자를 사용할 수 있다고 가정하면 각 단계마다 4개의 호출이 이루어지므로 시간복잡도는 다음과 같이 계산할 수 있습니다.
+\[ T(N) = 4^{(N-1)} \]
+즉, 시간복잡도는 \( O(4^N) \) 입니다.
